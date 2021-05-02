7875f169601e4b051abcc8ff2abfa344117cec8f
Martynas_Hoppenas <martynas.hoppenas@gmail.com>
2021-05-02 14:51:23 +0300

    timeconverter updated

--- src/utils/TimeConverter.jsx
+++ src/utils/TimeConverter.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
-
-// function TimeConverter(time) {
-//     const minutes = Math.floor((time / (1000 * 60)) % 60);
-//     const hours = Math.floor((time / (1000 * 60 * 60)) % 24);
-//     return (`${hours} : ${minutes}`)
-// };
-
 function TimeConverter(milli) {
   let time = new Date(milli);
   let hours = time.getUTCHours();
   let minutes = time.getUTCMinutes();
-  let seconds = time.getUTCSeconds();
-  let milliseconds = time.getUTCMilliseconds();
-  return hours + ":" + minutes + ":" + seconds;
+
+  if (hours<10) {
+      hours = "0" + hours.toString();
+  };
+
+  if (minutes<10) {
+      minutes = "0" + minutes.toString();
+  };
+
+
+  return hours + ":" + minutes;
 };
 
 export default TimeConverter;
